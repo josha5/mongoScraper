962f28250b4d0b64ba30d322e0c4ced144653d6f
Joshua Arnett <Josharnett850@gmail.com>
2018-05-22 01:09:44 -0400

    created delete route added read me

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mongoScraper
-mongo scraper app
+This is a full stack application that uses the mmanews.com website to scrape their articles and insert  them into a mongoDB database. Users are able to save articles and delete articles. An add notes feature to the saved articles is coming soon. View the deployed version on heroku at: https://josha5-mongoscraper.herokuapp.com/
+
+## Technologies Used
+* Node.js
+* express
+* mongoose
+* mongoDB
+* axios
+* cheerio
+* body-parser
+* request  
+* express-handlebars
+
+
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
-$("#scrapeArticles").on("click", function() { 
+$("#scrapeArticles").on("click", function(e) { 
+    e.preventDefault();
     $.getJSON("/articles", function(data) {
     // For each one
         for (var i = 0; i < data.length; i++) {
@@ -7,7 +8,7 @@ $("#scrapeArticles").on("click", function() {
             $("#articles").append(`<div class="card w-100 mt-3">
                                         <div class="card-body">
                                             <h3 class="card-title" data-id="${data[i]._id}"> ${data[i].title}</h3>
-                                            <h3><a href="${data[i].link}">Article Link: ${data[i].link}</a></h3>
+                                            <h3>Article Link:<a href="${data[i].link}"> ${data[i].link}</a></h3>
                                             <p class="card-text">Article Content Here.</p>
                                             <button type="button" id="saveBtn" data-id="${data[i]._id}" class="btn btn-primary">Save Article</button>
                                         </div>
@@ -21,21 +22,31 @@ $(document).on("click", "#saveBtn", function() {
 
     $.post("/index/" + thisID)
     .done(function(data) {
-        window.location = "/saved"
         console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            // Display the apropos information on the page
-           
-            $("#savedArticles").append(`<div class="card w-100 mt-3">
-                                        <div class="card-body">
-                                            <h3 class="card-title" data-id="${data[i]._id}"> ${data[i].title}</h3>
-                                            <h3><a href="${data[i].link}">Article Link: ${data[i].link}</a></h3>
-                                            <p class="card-text">Article Content Here.</p>
-                                            <button type="button" id="addNoteBtn" class="btn btn-primary">Add Note</button>
-                                            <button type="button" id="deleteArticleBtn" class="btn btn-danger">Delete Article</button>
-                                        </div>
-                                    </div>`);
-        } 
     });
+});
+
+
+$(document).on("click", "#addNoteBtn", function () {
+    let thisID = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisID,
+        data: {
+          text: $("#noteText" + thisID).val()
+        }
+      }).done(function(data) {
+          console.log(data);
+          $("#noteText" + thisID).val("");
+          $(".noteModal").modal("hide");
+      });
+});
+
+$(document).on("click", "#deleteArticleBtn", function() {
+        let thisID = $(this).attr("data-id");
+        $.post("/articles/delete/" + thisID)
+        .done(function(data) {
+            window.location = "/saved";
+        })
 });
 
